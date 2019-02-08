ca88892fa7cb382fe135a550ce8c73c172c57528
hungHoangDang <hungdh131@gmail.com>
2019-02-08 17:42:21 +0700

    change app for heroku deployment

--- app.js
+++ app.js
@@ -63,4 +63,4 @@ app.use('/*', (req, res) => {
 // });
 require('./prod')(app);
 
-app.listen(3000 || process.env.PORT, () => console.log('Listening...'));
+app.listen(3000 || process.env.PORT, "0.0.0.0", () => console.log('Listening...'));
